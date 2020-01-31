47219cfee35702ad992ea00f344dd9c57c0eba16
muhammadhafizm <m.hafizmaulana777@gmail.com>
2020-01-31 11:49:59 +0700

    portfolio selsai

---
+++ style/navbar.js
@@ -0,0 +1,21 @@
+//navbar function
+function tooglebtn1() {
+    var btn = document.getElementById("mobile-menu-button");
+    var nav = document.getElementById("navbar");
+    var ops = document.getElementById("select");
+    btn.classList.toggle("active");
+    ops.classList.toggle("active");
+    nav.classList.toggle("active");
+ };
+ function lineover(j){
+     var line = document.getElementsByClassName("nav-line");
+     var sel = document.getElementsByClassName("nav-select");
+         line[j].classList.add("active");
+         sel[j].classList.add("active");
+ };
+ function lineout(j){
+     var line = document.getElementsByClassName("nav-line");
+     var sel = document.getElementsByClassName("nav-select");
+         line[j].classList.remove("active");
+         sel[j].classList.remove("active");
+ }
---
+++ style/scroll-up.js
@@ -0,0 +1,38 @@
+// scroll-up animation in home
+const arrow = document.querySelector(".scroll-up");
+const client = document.querySelector(".navbar");
+
+const clientOptions = {
+    rootMargin: ""
+};
+
+const clientObsever = new IntersectionObserver(function(
+    entries, 
+    clientObsever
+    ) {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                arrow.classList.add("active");
+            } else{
+                arrow.classList.remove("active")
+            }
+        })
+    }, clientOptions);
+
+clientObsever.observe(client);
+
+// porto animation in home
+
+function portoHover(j){
+    var overlay = document.getElementsByClassName("overlay");
+    for (var i = 0; i < overlay.length; i++){
+        overlay[i].classList.add("active");
+    }
+    overlay[j].classList.remove("active");
+}
+function portoOut(j){
+    var overlay = document.getElementsByClassName("overlay");
+    for (var i = 0; i < overlay.length; i++){
+        overlay[i].classList.remove("active");
+    }
+}
